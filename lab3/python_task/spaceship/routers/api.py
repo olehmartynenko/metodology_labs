6cc7d4ee358e44a229a8721af419d15f7c316b77
olehmartynenko <martynenkooleg77@gmail.com>
2023-03-31 13:50:29 +0300

    Add numpy dependency and endpoint

--- lab3/python_task/spaceship/routers/api.py
+++ lab3/python_task/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,18 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World! It is Martynenko Oleh from IM-11'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    a = np.random.rand(10, 10)
+    b = np.random.rand(10, 10)
+
+    result = np.dot(a, b)
+
+    response = {
+        "matrix_a": a.tolist(),
+        "matrix_b": b.tolist(),
+        "product": result.tolist()
+    }
+
+    return response
